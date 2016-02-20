d55de04372ae8de29f9a15253061f2555b2e9bea
cwalke11 <cwalke11@gmu.edu>
2016-02-20 14:14:06 -0500

    changed name of grid in maze.c

--- maze.c
+++ maze.c
@@ -5,13 +5,13 @@
 
 int main(){
 
-	grid_t* test = blank_grid(10, 10);
-//	test_maze_generator(test);
-//	link_north(test, 4, 4);
+	grid_t* grid = blank_grid(10, 10);
+//	test_maze_generator(grid);
+//	link_north(grid, 4, 4);
 //	link_east(tes
-//	binary_tree_generator(test);
-	side_winder_generator(test);
-	make_entrances(test);
-	print_grid(test);
+	binary_tree_generator(grid);
+//	side_winder_generator(grid);
+	make_entrances(grid);
+	print_grid(grid);
 
 }
